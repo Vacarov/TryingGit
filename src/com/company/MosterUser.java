b4b9ae928733af85fa51e89da6587bb801d4cfd8
Vacarov <vervac@mail.ru>
2017-07-07 22:25:50 +0300

    commit from looser . add in master and in monsterUser

--- src/com/company/MosterUser.java
+++ src/com/company/MosterUser.java
@@ -4,4 +4,32 @@ package com.company;
  * Created by wergin on 07-Jul-17.
  */
 public class MosterUser {
+    public MosterUser() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
 }
